c90be15a34ccf89e47fe1e129803460155608776
Job Gateri <job.gateri@dtone.com>
2024-04-22 19:53:06 +0300

    Changed the script to standard Emacs convention for package loading

--- customizations/setup-js.el
+++ customizations/setup-js.el
@@ -1,20 +1,23 @@
-;; javascript / html
-(setup (:package tagedit)
-  (:hook-into html-mode))
+;; JavaScript / HTML Configuration
+(require 'tagedit)
+(add-hook 'html-mode-hook #'tagedit-mode)
 
-(setup subword-mode
-  (:hook-into js-mode
-	      html-mode
-	      coffee-mode))
+(add-hook 'js-mode-hook #'subword-mode)
+(add-hook 'html-mode-hook #'subword-mode)
+(add-hook 'coffee-mode-hook #'subword-mode)
 
 (setq js-indent-level 2)
 
-;; coffeescript
-(setup coffee-mode
-  (:hook highlight-indentation-current-column-mode
-	 (defun coffee-mode-newline-and-indent ()
-	   (define-key coffee-mode-map "\C-j" 'coffee-newline-and-indent)
-	   (setq coffee-cleanup-whitespace nil))))
+;; CoffeeScript Configuration
+(require 'coffee-mode)
+(add-hook 'coffee-mode-hook #'highlight-indentation-current-column-mode)
+
+(defun coffee-mode-newline-and-indent ()
+  "Define key for newline and indentation in CoffeeScript mode."
+  (define-key coffee-mode-map (kbd "RET") 'coffee-newline-and-indent)
+  (setq coffee-cleanup-whitespace nil))
+
+(add-hook 'coffee-mode-hook #'coffee-mode-newline-and-indent)
 
 (custom-set-variables
  '(coffee-tab-width 2))
